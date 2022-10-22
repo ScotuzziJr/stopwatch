b9872011783237eace840edd698f71f9ae6c90c4
scotuzzijr <marcio@up4tech.com.br>
2022-10-22 13:13:02 -0300

    feat: stopwatch implementation

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// global variables
+const timeElement = document.querySelector('.watch .time')
+const startButton = document.getElementById('start')
+const stopButton  = document.getElementById('stop')
+const resetButton = document.getElementById('reset')
+
+let seconds  = 0
+let interval = null
+
+// event listeners
+startButton.addEventListener('click', start)
+stopButton.addEventListener('click', stop)
+resetButton.addEventListener('click', reset)
+
+// update timer
+function timer() {
+    seconds++
+
+    // format timer
+    let hrs  = Math.floor(seconds / 3600)
+    let mins = Math.floor((seconds - (hrs * 3600)) / 60)
+    let secs = seconds % 60
+
+    if (secs < 10) secs = '0' + secs
+    if (mins < 10) mins = '0' + mins
+    if (hrs < 10) hrs   = '0' + hrs
+
+    timeElement.innerText = `${hrs}:${mins}:${secs}`
+}
+
+function start() {
+    if (interval) { 
+    return 
+    }
+
+    interval = setInterval(timer, 1000)
+}
+
+function stop() {
+    clearInterval(interval)
+    interval = null
+}
+
+function reset() {
+    stop();
+    seconds = 0
+    timeElement.innerText = '00:00:00'
+}
